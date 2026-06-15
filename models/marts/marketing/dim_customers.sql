dbef584ccaa7591c580784bcef73e1eab6a58a4a
Victor Muljo <victormuljo@MacBookPro.lan>
2026-06-14 18:48:26 -0700

    testing ci

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -34,7 +34,7 @@ final as (
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
+        customer_orders.most_recent_order_date as latest_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
 
